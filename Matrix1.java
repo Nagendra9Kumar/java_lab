26c61b9d2a122d8b1a654c02f483cc745774b385
nagendra <nagendragubbala999@gmail.com>
2024-02-15 21:35:58 +0530

    add some changes to matrix1

--- Matrix1.java
+++ Matrix1.java
@@ -1,6 +1,7 @@
 import java.util.*;
+
 public class Matrix1 {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter M value: ");
         int m = sc.nextInt();
@@ -10,23 +11,24 @@ public class Matrix1 {
         int m2[][] = new int[m][n];
         int result[][] = new int[m][n];
         System.out.println("Enter Elements of M1 matrix: ");
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                m1[i][j]=sc.nextInt();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                m1[i][j] = sc.nextInt();
             }
         }
         System.out.println("Enter Elements of M2 matrix: ");
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                m2[i][j]=sc.nextInt();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                m2[i][j] = sc.nextInt();
             }
         }
-        System.out.println("Result: ");
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                result[i][j]=(m1[i][j]+m2[i][j]);
-                System.out.print(result[i][j]+" ");
+        System.out.print("Result: ");
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                result[i][j] = (m1[i][j] + m2[i][j]);
+                System.out.print(result[i][j] + " ");
             }
         }
+        sc.close();
     }
 }
